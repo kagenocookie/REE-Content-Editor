14ec1b4b88ffbfb7cfdc2029152a0966f8a2ec8c
shadowcookie <45361488+kagenocookie@users.noreply.github.com>
2025-10-07 22:47:00 +0200

    Remove error message for empty meshes

--- ContentEditor.App/Graphics/OpenGLRenderContext.cs
+++ ContentEditor.App/Graphics/OpenGLRenderContext.cs
@@ -137,7 +137,7 @@ public sealed class OpenGLRenderContext(GL gl) : RenderContext
 
         var handle = new MeshResourceHandle(MeshRefs.NextInstanceID);
         var meshFile = meshResource.NativeMesh;
-        if (meshFile.MeshData == null) {
+        if (meshFile.MeshData == null || !(meshFile.MeshBuffer?.Positions.Length > 0)) {
             // TODO occluder meshes
             return handle;
         }
