daf1e895ec834fe71da210011d9ab4e8b4f12026
shadowcookie <45361488+kagenocookie@users.noreply.github.com>
2025-10-13 23:28:35 +0200

    Add a CompositeRigidBodyMeshSet ToString override

---
+++ ContentPatcher/RszFieldOverrides/RigidBodyFields.cs
@@ -0,0 +1,28 @@
+using System.Numerics;
+using ContentPatcher;
+using ReeLib;
+using ReeLib.via;
+
+namespace ContentPatcher;
+
+public static partial class RszFieldCache
+{
+    /// <summary>
+    /// via.dynamics.CompositeRigidBodyMeshSet.MeshInfo
+    /// </summary>
+    public static class CompositeRigidBodyMeshSet
+    {
+        [RszAccessor("via.dynamics.CompositeRigidBodyMeshSet.MeshInfo")]
+        public static class MeshInfo
+        {
+            public static readonly RszFieldAccessorFirst<Vector3> Position = First<Vector3>(f => f.type is RszFieldType.Vec3 || f.size == 16)
+                .Type(RszFieldType.Vec3)
+                .Rename()
+                .Optional();
+            public static readonly RszFieldAccessorFirst<string> Resource = First<string>(f => f.type is RszFieldType.String or RszFieldType.Resource)
+                .Resource("via.dynamics.DynamicsBaseResourceHolder")
+                .Rename()
+                .Optional();
+        }
+    }
+}
