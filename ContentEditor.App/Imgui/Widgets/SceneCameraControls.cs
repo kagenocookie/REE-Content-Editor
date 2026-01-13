22e1f237e8edb274cb49f28751925f090c07b13c
SilverEzredes <silverezredes@gmail.com>
2026-01-13 18:38:37 +0100

    Tweak widgets, adjust RE Engine theme colors

--- ContentEditor.App/Imgui/Widgets/SceneCameraControls.cs
+++ ContentEditor.App/Imgui/Widgets/SceneCameraControls.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using ContentEditor.App.ImguiHandling;
+using ContentEditor.Core;
 
 namespace ContentEditor.App;
 
@@ -20,12 +21,14 @@ public class SceneCameraControls : ISceneWidget
         var style = ImGui.GetStyle();
         var btnSize = style.FramePadding * 2 + new Vector2(UI.FontSize, UI.FontSize);
         btnSize.X = (btnSize.X + style.FramePadding.X) + style.FramePadding.X;
-        ImGui.SetNextWindowPos(offset + new Vector2(size.X - (btnSize.X - 8) * 3, 8));
+        ImGui.SetNextWindowPos(offset + new Vector2(size.X - (btnSize.X - 12) * 3, 2));
         ImGui.BeginChild(WidgetName, new Vector2(btnSize.X, btnSize.Y + style.FramePadding.Y * 2));
         ImGui.SetCursorPos(ImGui.GetCursorPos() + style.FramePadding);
-        if (ImGui.Button($"{AppIcons.GameObject}")) {
+        if (ImGui.Button($"{AppIcons.SI_GenericCamera}")) {
             ImGui.OpenPopup(WidgetName);
         }
+        ImguiHelpers.Tooltip("Camera Controls");
+
         if (ImGui.BeginPopup(WidgetName)) {
             scene.Controller.ShowCameraControls();
             if (Math.Abs(scene.Controller.MoveSpeed - AppConfig.Settings.SceneView.MoveSpeed) > 0.001f) {
