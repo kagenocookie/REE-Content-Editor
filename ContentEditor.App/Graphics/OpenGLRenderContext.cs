b68642dd276b0dae432e7efc59ef20b0f5a074e2
SilverEzredes <silverezredes@gmail.com>
2026-01-19 12:38:13 +0100

    Adjust MCOL color Vol.2

--- ContentEditor.App/Graphics/OpenGLRenderContext.cs
+++ ContentEditor.App/Graphics/OpenGLRenderContext.cs
@@ -72,7 +72,7 @@ public sealed class OpenGLRenderContext(GL gl) : RenderContext
         if (!builtInMaterials.TryGetValue((BuiltInMaterials.Wireframe, flags), out var material)) {
             material = new(GL, GetShader("Shaders/GLSL/wireframe.glsl", flags));
             material.SetParameter("_OuterColor", new Color(0, 0, 0, 5));
-            material.SetParameter("_InnerColor", Colors.FileTypeMCOL with { Y = 1});
+            material.SetParameter("_InnerColor", Colors.FileTypeMCOL);
             material.name = "wire";
         }
         return material.Clone();
