af8f38d3d8aec1a8e7981d56ba298891a81bbe84
SilverEzredes <silverezredes@gmail.com>
2026-02-17 11:04:44 +0100

    Move first time setup logic to Home Page

--- ContentEditor.App/Imgui/App/Home.cs
+++ ContentEditor.App/Imgui/App/Home.cs
@@ -1,5 +1,6 @@
 using ContentEditor.App.Windowing;
 using ContentEditor.Core;
+using ContentEditor.Themes;
 using ReeLib;
 using Silk.NET.Maths;
 using System.Numerics;
@@ -16,10 +17,16 @@ public class HomeWindow : IWindowHandler
     private static HashSet<string>? fullSupportedGames;
     private string recentFileFilter = string.Empty;
     private bool isRecentFileFilterMatchCase = false;
+    private static string[] gameNames = null!;
+    private static string[] gameNameCodes = null!;
+    private string chosenGame = "";
+    private bool customGame;
     public void Init(UIContext context)
     {
         this.context = context;
         data = context.Get<WindowData>();
+        gameNameCodes = AppConfig.Instance.GetGamelist().Select(gs => gs.name).ToArray();
+        gameNames = gameNameCodes.Select(code => Languages.TranslateGame(code)).ToArray();
     }
     public void OnWindow() => this.ShowDefaultWindow(context);
     public void OnIMGUI()
@@ -82,6 +89,7 @@ public class HomeWindow : IWindowHandler
         ImGui.Spacing();
         ImGui.Separator();
         ImGui.Spacing();
+
         fullSupportedGames ??= ResourceRepository.RemoteInfo.Resources.Where(kv => kv.Value.IsFullySupported).Select(kv => kv.Key).ToHashSet();
         var games = AppConfig.Instance.GetGamelist();
         var currentActiveGame = EditorWindow.CurrentWindow?.Workspace.Env.Config.Game.name;
@@ -102,13 +110,12 @@ public class HomeWindow : IWindowHandler
                 ImGui.Separator();
             }
         }
-
         ImGui.Spacing();
         ImGui.Separator();
         ImGui.Spacing();
-        float footerHeight = 0;
+
         float remainingSpace = ImGui.GetContentRegionAvail().Y;
-        footerHeight += ImGui.GetFrameHeight() + ImGui.GetStyle().ItemSpacing.Y + ImGui.GetTextLineHeightWithSpacing() * 3;
+        float footerHeight = ImGui.GetFrameHeight() + ImGui.GetStyle().ItemSpacing.Y + ImGui.GetTextLineHeightWithSpacing() * 3;
         if (remainingSpace > footerHeight) ImGui.SetCursorPosY(ImGui.GetCursorPosY() + (remainingSpace - footerHeight));
         if (ImGui.Button($"{AppIcons.SI_GenericHeart} Support Development", new Vector2(ImGui.GetContentRegionAvail().X, 0))) {
             FileSystemUtils.OpenURL("https://ko-fi.com/shadowcookie");
@@ -132,9 +139,9 @@ public class HomeWindow : IWindowHandler
         ImGui.Spacing();
         ImGui.Separator();
         ImGui.Spacing();
-        ImGui.PushStyleColor(ImGuiCol.Text, Colors.IconActive);
+        ImGui.PushStyleColor(ImGuiCol.Text, Colors.TextActive);
         // SILVER: Maybe even a notification when a new version is up?
-        ImGui.Text("[AppVersion/UpdateTime]");
+        ImGui.Text("Version: " + AppConfig.Version);
         ImGui.PopStyleColor();
     }
     private static void ShowWelcomeText()
@@ -153,16 +160,100 @@ public class HomeWindow : IWindowHandler
         ImGui.PopFont();
     }
 
-    private static void ShowTabs()
+    private void ShowTabs()
     {
         if (ImGui.BeginTabBar("HomeTabs")) {
-            if (ImGui.BeginTabItem("Bundles")) {
-                // TODO SILVER: I of course totally know how to do this 100% ref: https://github.com/WolvenKit/WolvenKit?tab=readme-ov-file#screenshots
-                ImGui.Text("In a grid with the app logo being used as the image/icon.\nCould be color coded by game.");
+            if (AppConfig.Instance.IsFirstTime && ImGui.BeginTabItem("First Time Setup")) {
+                ImGui.SeparatorText("Choose a theme and color");
+                var theme = AppConfig.Instance.Theme.Get();
+                if (ImguiHelpers.ValueCombo("Theme", DefaultThemes.AvailableThemes, DefaultThemes.AvailableThemes, ref theme)) {
+                    UI.ApplyTheme(theme!);
+                    AppConfig.Instance.Theme.Set(theme);
+                }
+                ImguiHelpers.Tooltip("You can modify or create new custom themes through Edit > Theme Editor.");
+
+                var color = AppConfig.Instance.BackgroundColor.Get().ToVector4();
+                if (ImGui.ColorEdit4("Scene Background Color", ref color)) {
+                    var newColor = ReeLib.via.Color.FromVector4(color);
+                    AppConfig.Instance.BackgroundColor.Set(newColor);
+                    foreach (var wnd in MainLoop.Instance.Windows) {
+                        wnd.ClearColor = newColor;
+                    }
+                }
+                ImguiHelpers.Tooltip("You can change this color at any time in Settings > Display > Theme.");
+
+                ImGui.SeparatorText("Choose the game you wish to mod");
+
+                ImGui.Checkbox("Custom Game", ref customGame);
+                ImguiHelpers.Tooltip("Select this if you wish to configure a game outside of the predefined list.\nCustom games may not fully work.");
+                if (customGame) {
+
+                    if (!string.IsNullOrEmpty(chosenGame) && !Enum.TryParse<GameName>(chosenGame, out _)) {
+                        ImGui.SameLine();
+                        ImGui.Button($"{AppIcons.SI_GenericInfo}");
+                        ImguiHelpers.TooltipColored("This is a custom defined game. The app may need an upgrade to fully support all files, some files may not load correctly.", Colors.Note);
+                    }
+                    ImGui.InputText("Game Short Name", ref chosenGame, 20);
+                    ImGui.SameLine();
+                    ImGui.TextColored(Colors.TextActive, "*");
+                    chosenGame = chosenGame.Replace(" ", "");
+                } else {
+                    ImguiHelpers.ValueCombo("##Game", gameNames, gameNameCodes, ref chosenGame);
+                    ImGui.SameLine();
+                    ImGui.TextColored(Colors.TextActive, "*");
+                }
+                if (!string.IsNullOrEmpty(chosenGame)) {
+                    var gamepath = AppConfig.Instance.GetGamePath(chosenGame);
+                    var rszPath = AppConfig.Instance.GetGameRszJsonPath(chosenGame);
+                    var filelist = AppConfig.Instance.GetGameFilelist(chosenGame);
+                    var extractPath = AppConfig.Instance.GetGameExtractPath(chosenGame);
+                    var isCustomGame = !Enum.TryParse<GameName>(chosenGame, out _);
+
+                    if (AppImguiHelpers.InputFolder("Game Path", ref gamepath) && Directory.Exists(gamepath)) {
+                        AppConfig.Instance.SetGamePath(chosenGame, gamepath);
+                    }
+                    ImguiHelpers.Tooltip("This is the path to the game (where the .exe file is located).");
+                    ImGui.SameLine();
+                    ImGui.TextColored(Colors.TextActive, "*");
+
+                    if (isCustomGame) {
+                        if (AppImguiHelpers.InputFilepath("RSZ JSON File Path", ref rszPath, FileFilters.JsonFile) && File.Exists(gamepath)) {
+                            AppConfig.Instance.SetGameRszJsonPath(chosenGame, rszPath);
+                        }
+                        ImguiHelpers.Tooltip("This setting should point to the correct rsz*.json for the chosen game.");
+
+                        if (AppImguiHelpers.InputFilepath("File List Path", ref filelist) && File.Exists(gamepath)) {
+                            AppConfig.Instance.SetGameFilelist(chosenGame, filelist);
+                        }
+                        ImguiHelpers.Tooltip("This setting should point to a filepath containing a list of all files used by the game.");
+
+                        if (AppImguiHelpers.InputFilepath("File Extraction Path", ref extractPath) && File.Exists(gamepath)) {
+                            AppConfig.Instance.SetGameExtractPath(chosenGame, extractPath);
+                        }
+                        ImguiHelpers.Tooltip("This is the default path used when extracting files. Can be left empty.");
+                    }
+                }
+                ImGui.SameLine();
+                string finishText = "Finish Setup";
+                ImguiHelpers.AlignElementRight(ImGui.CalcTextSize(finishText).X + ImGui.GetStyle().ItemSpacing.X + ImGui.GetStyle().FramePadding.X);
+                using (var _ = ImguiHelpers.Disabled((string.IsNullOrEmpty(chosenGame) || string.IsNullOrEmpty(AppConfig.Instance.GetGamePath(chosenGame))))) {
+                    if (ImGui.Button(finishText)) {
+                        AppConfig.Instance.IsFirstTime.Set(false);
+                        EditorWindow.CurrentWindow?.SetWorkspace(chosenGame, null);
+                    }
+                }
+                ImGui.SeparatorText("##LoremIpsum");
                 ImGui.EndTabItem();
             }
-            if (ImGui.BeginTabItem("Update Notes")) {
-                ImGui.EndTabItem();
+            if (!AppConfig.Instance.IsFirstTime) {
+                if (ImGui.BeginTabItem("Bundles")) {
+                    // TODO SILVER: I of course totally know how to do this 100% ref: https://github.com/WolvenKit/WolvenKit?tab=readme-ov-file#screenshots
+                    ImGui.Text("In a grid with the app logo being used as the image/icon.\nCould be color coded by game.");
+                    ImGui.EndTabItem();
+                }
+                if (ImGui.BeginTabItem("Updates")) {
+                    ImGui.EndTabItem();
+                }
             }
             ImGui.EndTabBar();
         }
@@ -173,6 +264,7 @@ public class HomeWindow : IWindowHandler
         ImguiHelpers.ToggleButton($"{AppIcons.SI_GenericMatchCase}", ref isRecentFileFilterMatchCase, Colors.IconActive);
         ImguiHelpers.Tooltip("Match Case");
         ImGui.SameLine();
+        ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
         ImGui.SetNextItemAllowOverlap();
         ImGui.InputTextWithHint("##RecentFileFilter", $"{AppIcons.SI_GenericMagnifyingGlass} Search Recent Files", ref recentFileFilter, 128);
         if (!string.IsNullOrEmpty(recentFileFilter)) {
