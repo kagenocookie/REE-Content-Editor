ee5974ccbf7df6429dabc94f9b069defd4d67dca
shadowcookie <45361488+kagenocookie@users.noreply.github.com>
2026-06-04 20:06:15 +0200

    Add additional onimusha wots file support

---
+++ ContentPatcher/FileLoaders/Chain2LodFileLoader.cs
@@ -0,0 +1,7 @@
+using ReeLib;
+
+namespace ContentPatcher;
+
+public class Chain2LodFileLoader() : DefaultFileLoader<Chain2LodFile>(KnownFileFormats.Chain2Lod)
+{
+}
---
+++ ContentPatcher/FileLoaders/SvgSeqFileLoader.cs
@@ -0,0 +1,7 @@
+using ReeLib;
+
+namespace ContentPatcher;
+
+public class SvgSeqFileLoader() : DefaultFileLoader<SvgSqFile>(KnownFileFormats.SVGSequence)
+{
+}
